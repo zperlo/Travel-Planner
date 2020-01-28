8f044cb411fd85cc27e98f130fa5b7a34866d2b1
Dylan Briggs <dhb48@case.edu>
2020-01-28 17:46:00 -0500

    added a comment to settings.py for testing git branching

--- djangoTravelPlanner/djangoTravelPlanner/settings.py
+++ djangoTravelPlanner/djangoTravelPlanner/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+#Dylan adding a test comment here
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
