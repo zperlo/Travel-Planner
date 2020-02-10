1ed90034b44796a6482f362fb9a9d46c901a7628
Jacob Rich <cjr85@case.edu>
2020-02-10 11:21:39 -0500

    switched from path to re_path in urls.py, allowing regex

--- djangoTravelPlanner/djangoTravelPlanner/urls.py
+++ djangoTravelPlanner/djangoTravelPlanner/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path(r'', views.homepage),
+    re_path(r'^$', views.homepage),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
