d8fde3d06b770f97e9e4d7f774b769a45b3ea156
Jacob Rich <cjr85@case.edu>
2020-02-10 10:59:14 -0500

    added background color as proof of concept for static files

--- djangoTravelPlanner/djangoTravelPlanner/urls.py
+++ djangoTravelPlanner/djangoTravelPlanner/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path(r'', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- djangoTravelPlanner/djangoTravelPlanner/views.py
+++ djangoTravelPlanner/djangoTravelPlanner/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def homepage(request):
     return render(request, 'homepage.html')
 
