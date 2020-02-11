20fb30bf496b7de8059a43a2a048afcb5b6029a2
Dylan Briggs <dhb48@case.edu>
2020-02-11 18:14:18 -0500

    adding basic javascript functionality

--- djangoTravelPlanner/Frontend/script.js
+++ djangoTravelPlanner/Frontend/script.js
@@ -1,2 +1,14 @@
 let d = new Date();
-alert("Today's date is " + d);
+//alert("Today's date is " + d);
+
+function displayCity() {
+    alert("City: " + document.getElementById("where").value);
+}
+
+// Cannot disable the entire form all at once
+function disableForm() {
+    document.getElementById("where").disabled = true;
+    document.getElementById("date").disabled = true;
+    document.getElementById("start").disabled = true;
+    document.getElementById("end").disabled = true;
+}
