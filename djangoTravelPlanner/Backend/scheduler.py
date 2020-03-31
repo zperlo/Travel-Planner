e62d05231d73fd3c61743a497c7db23177df6896
Tyler Anderson <twa16@case.edu>
2020-03-31 15:38:14 -0400

    It's ALIVE
    We have a working scheduler

--- djangoTravelPlanner/Backend/scheduler.py
+++ djangoTravelPlanner/Backend/scheduler.py
@@ -1,5 +1,7 @@
 import datetime
 import yelpRequests as yelp
+from numpy.random import choice
+
 
 # makes business["hours"] into a copy of the schedule where each 
 # time increment is True if the business is open, and false if not
@@ -26,6 +28,7 @@ def formatOpenCloseSchedule(business, hours, yelpStartTime, startDay):
                 if currDay > 6:
                     currDay = 0
 
+
 def createSchedule(businesses, travelTimes, startDate, endDate, startDay, endDay):
     # assumes startDate and endDate are datetime objects
     # assumes startDay and endDay are integers representing day of the week 0-6 Monday to Sunday, matching Yelp's API
@@ -44,10 +47,49 @@ def createSchedule(businesses, travelTimes, startDate, endDate, startDay, endDay
         if "id" in business.keys(): # does not need to do for starting location
             hours = yelp.getYelpHoursForBusiness(business["id"])
             formatOpenCloseSchedule(business, hours, startTime_YelpFormatted, startDay)
-        #print(business["name"] + ": ")
-        #print(business["hours"])
+        # print(business["name"] + ": ")
+        # print(business["hours"])
 
     # Call helper to perform algorithm X times and take best result
+    numRunTimes = 10  # make smaller if slow as sin
+    
+    bestSchedule = multiScheduler(businesses, travelTimes, schedule, numRunTimes, minutesPerIncrement)
+    
+    return bestSchedule
+
+
+def multiScheduler(businesses, travelTimes, schedule, numRunTimes, minutesPerIncrement):
+    schedules = [None]*numRunTimes
+    
+    for i in range(numRunTimes):
+        s = scheduleHelper(businesses, travelTimes, schedule.copy(), minutesPerIncrement)
+        schedules[i] = s
+    
+    maxVal = 0.0
+    maxValSchedule = None
+    for s in schedules:
+        s_val = 0.0
+        businessesAdded = []
+        
+        for increment in s:
+            
+            if increment != -1 and increment not in businessesAdded:
+
+                val = businesses[increment]['value']
+                # print('Val: ')
+                # print(val)
+                s_val += val
+                businessesAdded.append(increment)
+        
+        if s_val >= maxVal:
+            maxVal = s_val
+            maxValSchedule = s
+            
+        # print(s_val)
+        # print()
+        # print(s)
+
+    return maxValSchedule
 
 
 # Semi-random Greedy algorithm
@@ -57,69 +99,90 @@ def createSchedule(businesses, travelTimes, startDate, endDate, startDay, endDay
 # Do this until no activity can be scheduled or the end of the schedule is reached. 
 # If the end of the schedule is not reached, check each time until some activity can 
 # be scheduled, and repeat from the top.
+def scheduleHelper(businesses, travelTimes, schedule, minutesPerIncrement):
+    usedBusinesses = [0]
+
+    numIncrements = len(schedule)
+    
+    prevBusinessIndex = 0
+    incrementCounter = 0
+    while (incrementCounter < numIncrements):
+        # randomly choose a business
+        businessIndex, travelTime, timeSpent = randomBusiness(businesses, usedBusinesses, prevBusinessIndex, travelTimes, incrementCounter, minutesPerIncrement)
+        
+        if businessIndex != -1:
+            prevBusinessIndex = businessIndex
+            usedBusinesses.append(businessIndex)
+            
+            startIncrement = int(incrementCounter + travelTime)
+            endIncrement = int(startIncrement + timeSpent)
+            
+            for i in range(startIncrement, endIncrement):
+                schedule[i] = businessIndex
+            
+            incrementCounter = endIncrement
+            
+        else:
+            incrementCounter += 1
+    
+    return schedule
+
+
+# Given a list of business dictionaries, randomly choose a business
+# where the chance to pick a business is 0 if it cannot be done at the currentTime
+# and is proportional to its value if available
+def randomBusiness(businesses, usedBusinesses, prevBusinessIndex, travelTimes, currentTime, minutesPerIncrement):
+    businessWeights = [0]*len(businesses)
+
+    # loop through business and check for
+    # true at this time + travel time AND true until time + travel time + timeToSpend
+    for index, business in enumerate(businesses):
+        if index not in usedBusinesses:
+        
+            businessHours = business['hours']
+            timeToSpend = int(business['timeToSpend'] / minutesPerIncrement)
+            businessTravelTime = int(travelTimes[prevBusinessIndex][index] / minutesPerIncrement)
+            
+            checkTime = (currentTime + businessTravelTime)
+            if checkTime < len(businessHours) and businessHours[checkTime]:
+
+                tempTime = currentTime + businessTravelTime
+                while tempTime <= currentTime + businessTravelTime + timeToSpend and tempTime < len(businessHours):
+
+                    if not businessHours[tempTime]:
+                        break
+                    else:
+                        if tempTime == currentTime + businessTravelTime + timeToSpend:
+                            businessWeights[index] = business['value']        
+                        
+                        tempTime += 1
+
+    sumWeights = sum(businessWeights)
+    if sumWeights == 0:
+        return -1, -1, -1
+    
+    normalizedBusinessWeights = []
+    for weight in businessWeights:
+        normalizedBusinessWeights.append(weight / sumWeights)
+    
+    # randomly choose
+    businessIndex = choice(range(len(businesses)), 1, p=normalizedBusinessWeights)[0]
+    travelTime = travelTimes[prevBusinessIndex][businessIndex] / minutesPerIncrement
+    timeSpent = businesses[businessIndex]['timeToSpend'] / minutesPerIncrement
+
+    return businessIndex, travelTime, timeSpent
 
 
 x = datetime.datetime(2020, 3, 22)
-y = datetime.datetime.now()
-bus = [{'name': 'The Jolly Scholar', 'timeToSpend': 1, 'address': 'Thwing Ctr 11111 Euclid Ave Cleveland, OH 44106', 'rating': 4.0, 'id': 'wzj2cMpiDJW0HB3iCvCOYA', 'price': '$', 'url': 'https://www.yelp.com/biz/the-jolly-scholar-cleveland?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 4.0}, {'name': 'ABC the Tavern', 'timeToSpend': 1, 'address': '11434 Uptown Ave Cleveland, OH 44106', 'rating': 3.5, 'id': 'uYl_QBtb7bXhu9sgb4kCrg', 'price': '$', 'url': 'https://www.yelp.com/biz/abc-the-tavern-cleveland-4?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 3.5}, {'name': 'The Fairmount', 'timeToSpend': 1, 'address': '2448 Fairmount Blvd Cleveland Heights, OH 44106', 'rating': 4.0, 'id': 'g5zVkPRW2umfpCuDkan7tQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/the-fairmount-cleveland-heights?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 2.0}, {'name': 'Townhall', 'timeToSpend': 1, 'address': '1909 W 25th St Cleveland, OH 44113', 'rating': 4.0, 'id': 'LNsZJP6jZ11e0tDljOLPiQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/townhall-cleveland-2?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 2.0}, {'name': 'Punch Bowl Social Cleveland', 'timeToSpend': 1, 'address': '1086 W 11th St Cleveland, OH 44113', 'rating': 3.5, 'id': '9SrZRDl7-ZfuENCo0DjfsQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/punch-bowl-social-cleveland-cleveland?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 1.75}]
+y = datetime.datetime(2020, 3, 24)
+bus = [{'name': 'Start', 'address':'1611 E 115th St Cleveland, OH 44106'},
+       {'name': 'The Jolly Scholar', 'timeToSpend': 60, 'address': 'Thwing Ctr 11111 Euclid Ave Cleveland, OH 44106', 'rating': 4.0, 'id': 'wzj2cMpiDJW0HB3iCvCOYA', 'price': '$', 'url': 'https://www.yelp.com/biz/the-jolly-scholar-cleveland?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 4.0}, 
+       {'name': 'ABC the Tavern', 'timeToSpend': 60, 'address': '11434 Uptown Ave Cleveland, OH 44106', 'rating': 3.5, 'id': 'uYl_QBtb7bXhu9sgb4kCrg', 'price': '$', 'url': 'https://www.yelp.com/biz/abc-the-tavern-cleveland-4?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 3.5}, 
+       {'name': 'The Fairmount', 'timeToSpend': 60, 'address': '2448 Fairmount Blvd Cleveland Heights, OH 44106', 'rating': 4.0, 'id': 'g5zVkPRW2umfpCuDkan7tQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/the-fairmount-cleveland-heights?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 2.0}, 
+       {'name': 'Townhall', 'timeToSpend': 60, 'address': '1909 W 25th St Cleveland, OH 44113', 'rating': 4.0, 'id': 'LNsZJP6jZ11e0tDljOLPiQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/townhall-cleveland-2?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 2.0}, 
+       {'name': 'Punch Bowl Social Cleveland', 'timeToSpend': 60, 'address': '1086 W 11th St Cleveland, OH 44113', 'rating': 3.5, 'id': '9SrZRDl7-ZfuENCo0DjfsQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/punch-bowl-social-cleveland-cleveland?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 1.75}]
 distances = [[0, 219, 228, 528, 1042, 961], [292, 0, 209, 356, 963, 973], [231, 127, 0, 408, 1090, 1047], [596, 521, 493, 0, 1281, 1237], [969, 1093, 1090, 1194, 0, 402], [1033, 1156, 1154, 1272, 423, 0]]
-createSchedule(bus, distances, x, y, 6, 1)
+sched = createSchedule(bus, distances, x, y, 6, 1)
 
-print("scheduler.py ran")
-
-# REMOVE THE FOLLOWING CODE WHEN NO LONGER USEFUL FOR OBSERVATION
-#
-# from pulp import *
-#
-# def createSchedule(businesses, travelTimes, startDate, endDate):
-#     problem = LpProblem("Schedule Activities", LpMaximize)
-#     globalStartTime = 0.0
-#     globalStartDay = startDate # TODO: capture day of week from start date
-#     globalEndTime = endDate # convert to number of seconds since start time
-#     globalEndDay = endDate # TODO: capture day of week from end date
-#
-#     # create enough variables for start and end time of each activity
-#     activityStartEnd = LpVariable.dicts("activityStartEnd", list(range(2*len(businesses))))
-#
-#     activities = []
-#     for i in range(len(businesses)):
-#         # variable for each activity to say if it is scheduled or not
-#         activities.append(LpVariable("activity " + str(i), cat="Binary"))
-#
-#     goal = sum([businesses[i]["value"] * activities[i] for i in range(len(businesses))])
-#     problem += goal
-#
-#     for i in range(len(businesses)):
-#         # activities must be scheduled later than global start + travel time and scheduled earlier than global end
-#         c1 = activityStartEnd[2*i] * activities[i] >= (globalStartTime + travelTimes[0][i+1]) * activities[i]
-#         c2 = activityStartEnd[2*i + 1] * activities[i] >= (globalStartTime + travelTimes[0][i+1]) * activities[i]
-#         problem += c1
-#         problem += c2
-#         c3 = activityStartEnd[2*i] * activities[i] <= globalEndTime * activities[i]
-#         c4 = activityStartEnd[2*i + 1] * activities[i] <= globalEndTime * activities[i]
-#         problem += c3
-#         problem += c4
-#         # must spend required amount of time at activity (or 0 if not scheduled)
-#         c5 = activityStartEnd[2*i + 1] - activityStartEnd[2*i] == businesses[i]["timeToSpend"] * activities[i]
-#         problem += c5
-#
-#         # constraints based on open/close times
-#
-#         # constraints based on travel times and non-overlapping
-#         '''
-#         for j in range(businesses):
-#             if j != i:
-#                 # x start must be after y end + travel time from y to x
-#                 activityStartEnd[2*i] * (activities[i] + activities[j] - 1) >= (activityStartEnd[2*j + 1] + travelTimes[j+1][i+1]) * (activities[i] + activities[j] - 1)
-#                 # OR y start must be after x end + travel time from x to y
-#                 activityStartEnd[2*j] * (activities[i] + activities[j] - 1) >= (activityStartEnd[2*i + 1] + travelTimes[i+1][j+1]) * (activities[i] + activities[j] - 1)
-#                 # OR one of them must be not scheduled -- included in above expressions
-#         '''
-#
-#     print(problem)
-#     problem.solve()
-#     print(LpStatus[problem.status])
-#
-# #bus = [{'name': 'The Jolly Scholar', 'timeToSpend': 1, 'address': 'Thwing Ctr 11111 Euclid Ave Cleveland, OH 44106', 'rating': 4.0, 'id': 'wzj2cMpiDJW0HB3iCvCOYA', 'price': '$', 'url': 'https://www.yelp.com/biz/the-jolly-scholar-cleveland?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 4.0}, {'name': 'ABC the Tavern', 'timeToSpend': 1, 'address': '11434 Uptown Ave Cleveland, OH 44106', 'rating': 3.5, 'id': 'uYl_QBtb7bXhu9sgb4kCrg', 'price': '$', 'url': 'https://www.yelp.com/biz/abc-the-tavern-cleveland-4?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 3.5}, {'name': 'The Fairmount', 'timeToSpend': 1, 'address': '2448 Fairmount Blvd Cleveland Heights, OH 44106', 'rating': 4.0, 'id': 'g5zVkPRW2umfpCuDkan7tQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/the-fairmount-cleveland-heights?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 2.0}, {'name': 'Townhall', 'timeToSpend': 1, 'address': '1909 W 25th St Cleveland, OH 44113', 'rating': 4.0, 'id': 'LNsZJP6jZ11e0tDljOLPiQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/townhall-cleveland-2?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 2.0}, {'name': 'Punch Bowl Social Cleveland', 'timeToSpend': 1, 'address': '1086 W 11th St Cleveland, OH 44113', 'rating': 3.5, 'id': '9SrZRDl7-ZfuENCo0DjfsQ', 'price': '$$', 'url': 'https://www.yelp.com/biz/punch-bowl-social-cleveland-cleveland?adjust_creative=dQvSn56-m0sDVZemOiNI2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=dQvSn56-m0sDVZemOiNI2w', 'value': 1.75}]
-# #distances = [[0, 219, 228, 528, 1042, 961], [292, 0, 209, 356, 963, 973], [231, 127, 0, 408, 1090, 1047], [596, 521, 493, 0, 1281, 1237], [969, 1093, 1090, 1194, 0, 402], [1033, 1156, 1154, 1272, 423, 0]]
-# #createSchedule(bus, distances, 0.0, 172800.0)
+print('Chosen Schedule:')
+print(sched)
