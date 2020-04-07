fba026fc4f69560d0329f0ee025051434a260c8f
Jacob Rich <cjr85@case.edu>
2020-04-07 03:57:39 -0400

    added showing/hiding more details. having trouble setting img src of img.yelpStars in more details

--- djangoTravelPlanner/Frontend/script.js
+++ djangoTravelPlanner/Frontend/script.js
@@ -57,26 +57,67 @@ function showMoreDetails(event) {
   var buttons = document.getElementsByClassName("buttonClass");
 
   for (var i = 0; i < buttons.length; i++) {
-    if (buttons[i].id.match(/^expand:[0-9]+/)) {
+    if (buttons[i].id.match(/^expand:[0-9]+$/)) {
       buttons[i].value = '>';
     }
   }
 
   var expandID = getEventID(event);
   var button = document.getElementById(expandID);
+  var details = document.getElementById('moreDetails');
   if (expandID != showMoreDetails.lastID) {
     button.value = '<';
     showMoreDetails.lastID = expandID;
-    // TODO: show moreDetails section of HTML
+    populateDetails(
+      "Spread Bagelry",
+      "3.5",
+      "538",
+      "$$",
+      "Breakfast & Brunch, Bagels, Sandwiches",
+      "262 S 20th St",
+      "Philadelphia, PA 19103",
+      "https://s3-media0.fl.yelpcdn.com/bphoto/NwenshmRDzryFAPVbXZgJg/o.jpg");
+    details.style.visibility = 'visible';
   }
   else {
     button.value = '>';
     showMoreDetails.lastID = "";
-    // TODO: hide moreDetails section of HTML
+    details.style.visibility = 'hidden';
   }
 
 }
 
 function getEventID(event) {
   return event.target.id;
+}
+
+function populateDetails(name, rating, reviewCount, price, categories, addressLine1, addressLine2, imageURL) {
+  var details = document.getElementById('moreDetails');
+  var divs = details.childNodes;
+
+  console.log('details children:');
+  console.log(divs);
+
+  var children = divs[1].childNodes;
+
+  console.log('div children:');
+  console.log(children);
+
+  // update name
+  children[1].textContent = name;
+
+  // update rating
+  children[3].src = getRatingImageStaticPath(rating);
+}
+
+function getRatingImageStaticPath(rating) {
+
+  var infix = rating.charAt(0);
+  if (rating.includes('.')) {
+    infix += "_half";
+  }
+
+  str = "{% static 'resources\\yelpStars\\large_".concat(infix, "@2x.png' %}");
+  console.log(str);
+  return str;
 }
