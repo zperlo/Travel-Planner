a6ef50018b1738a5039a67066da134e0f63551ae
Jacob Rich <cjr85@case.edu>
2020-04-09 14:03:58 -0400

    fixed a typo

--- djangoTravelPlanner/Frontend/script.js
+++ djangoTravelPlanner/Frontend/script.js
@@ -202,6 +202,6 @@ function noResults(search, city) {
  *    onClick or onBlur receives an event argument.
  */
 
-function destroyPreviousResult() {
+function destroyPreviousResults() {
   // TODO
 }
